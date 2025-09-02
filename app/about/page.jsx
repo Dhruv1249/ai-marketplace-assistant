5f6518ee6ef3ce59d9562601a1134983f547e11f
jainamjyoat <jainamjyoat2005@gmail.com>
2025-09-02 20:05:40 +0530

    added photo

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -27,14 +27,14 @@ const testimonials = [
       "Implementation was seamless and the results exceeded our expectations. The platform's flexibility is remarkable.",
     name: "Dhruv",
     designation: "Jainam Son",
-    src: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "/images/Dhruv.jpeg",
   },
   {
     quote:
       "This solution has significantly improved our team's productivity. The intuitive interface makes complex tasks simple.",
     name: "Yukta Shree",
     designation: "Food Lover",
-    src: "https://images.unsplash.com/photo-1623582854588-d60de57fa33f?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "/images/Yukta.jpeg",
   },
 ];
 
