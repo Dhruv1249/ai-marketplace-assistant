ba9f2465758015c799bd01d40952c86ddc9fa7d3
Dhruv1249 <dhr1249.lm@gmail.com>
2025-12-04 19:27:12 +0530

    removed eslint

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone', // Enable standalone output for Docker optimization
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   compiler: {
     styledComponents: true,
   },
